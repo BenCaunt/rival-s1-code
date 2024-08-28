a4d78322c971f490753e15bc1c9661fb10284dcc
Ben Caunt <bdcaunt@gmail.com>
2024-08-27 23:00:17 -0400

    more relevant example

--- main.py
+++ main.py
@@ -1,92 +1,50 @@
-#!/usr/bin/python3
 
-# Copyright 2023 mjbots Robotic Systems, LLC.  info@mjbots.com
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-'''Demonstrates using the RAW CAN API of the pi3hat.'''
+"""
+This example commands a single servo at ID #1 using the default
+transport to hold the current position indefinitely, and prints the
+state of the servo to the console.
+"""
 
 import asyncio
+import math
 import moteus
-import moteus_pi3hat
-import time
-
 
 async def main():
-    # The parameters of each CAN bus can be set at construction time.
-    # The available fields can be found in the C++ header at
-    # Pi3Hat::CanConfiguration
-    slow_can = moteus_pi3hat.CanConfiguration()
-    slow_can.slow_bitrate = 125000
-    slow_can.fdcan_frame = False
-    slow_can.bitrate_switch = False
+    # By default, Controller connects to id 1, and picks an arbitrary
+    # CAN-FD transport, prefering an attached fdcanusb if available.
+    c = moteus.Controller()
 
-    # If buses are not listed, then they default to the parameters
-    # necessary to communicate with a moteus controller.
-    can_config = {
-        5: slow_can,
-    }
-    transport = moteus_pi3hat.Pi3HatRouter(can=can_config)
-
-    # Since we didn't specify a 'servo_bus_map' to Pi3HatRouter, this
-    # will be assumed to be on bus 1.
-    controller = moteus.Controller(id = 1, transport=transport)
+    # In case the controller had faulted previously, at the start of
+    # this script we send the stop command in order to clear it.
+    await c.set_stop()
 
     while True:
-        # To send a raw CAN, you must manually instantiate a
-        # 'moteus.Command' and fill in its fields, along with which
-        # bus to send it on.
-        raw_message = moteus.Command()
-        raw_message.raw = True
-        raw_message.arbitration_id = 0x0405
-        raw_message.bus = 5
-        raw_message.data = b'1234'
-        raw_message.reply_required = False
-
-        # A single 'transport.cycle' call's message list can contain a
-        # mix of "raw" frames and those generated from
-        # 'moteus.Controller'.
+        # `set_position` accepts an optional keyword argument for each
+        # possible position mode register as described in the moteus
+        # reference manual.  If a given register is omitted, then that
+        # register is omitted from the command itself, with semantics
+        # as described in the reference manual.
         #
-        # If you want to listen on a CAN bus without having sent a
-        # command with 'reply_required' set, you can use the
-        # 'force_can_check' optional parameter.  It is a 1-indexed
-        # bitfield listing which additional CAN buses should be
-        # listened to.
+        # The return type of 'set_position' is a moteus.Result type.
+        # It has a __repr__ method, and has a 'values' field which can
+        # be used to examine individual result registers.
+        state = await c.set_position(position=math.nan, query=True)
 
-        results = await transport.cycle([
-            raw_message,
-            controller.make_query(),
-        ], force_can_check = (1 << 5))
+        # Print out everything.
+        print(state)
 
-        # If any raw CAN frames are present, the result list will be a
-        # mix of moteus.Result elements and can.Message elements.
-        # They each have the 'bus', 'arbitration_id', and 'data'
-        # fields.
-        #
-        # moteus.Result elements additionally have an 'id' field which
-        # is the moteus servo ID and a 'values' field which reports
-        # the decoded response.
-        for result in results:
-            if hasattr(result, 'id'):
-                # This is a moteus structure.
-                print(f"{time.time():.3f} MOTEUS {result}")
-            else:
-                # This is a raw structure.
-                print(f"{time.time():.3f} BUS {result.bus}  " +
-                      f"ID {result.arbitration_id:x}  DATA {result.data.hex()}")
+        # Print out just the position register.
+        print("Position:", state.values[moteus.Register.POSITION])
 
-        await asyncio.sleep(1.0)
+        # And a blank line so we can separate one iteration from the
+        # next.
+        print()
 
+        # Wait 20ms between iterations.  By default, when commanded
+        # over CAN, there is a watchdog which requires commands to be
+        # sent at least every 100ms or the controller will enter a
+        # latched fault state.
+        await asyncio.sleep(0.02)
 
 if __name__ == '__main__':
     asyncio.run(main())

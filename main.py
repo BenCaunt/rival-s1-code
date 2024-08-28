a69c804d21544f971ee3bc7cbc609d7be01e17bd
Ben Caunt <bdcaunt@gmail.com>
2024-08-27 23:08:33 -0400

    multi servo example

--- main.py
+++ main.py
@@ -1,50 +1,134 @@
+#!/usr/bin/python3 -B
+
+# Copyright 2023 mjbots Robotic Systems, LLC.  info@mjbots.com
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 """
-This example commands a single servo at ID #1 using the default
-transport to hold the current position indefinitely, and prints the
-state of the servo to the console.
+This example commands multiple servos connected to a pi3hat.  It
+uses the .cycle() method in order to optimally use the pi3hat
+bandwidth.
 """
 
 import asyncio
 import math
 import moteus
+import moteus_pi3hat
+import time
 
 async def main():
-    # By default, Controller connects to id 1, and picks an arbitrary
-    # CAN-FD transport, prefering an attached fdcanusb if available.
-    c = moteus.Controller()
+    # We will be assuming a system where there are 4 servos, each
+    # attached to a separate pi3hat bus.  The servo_bus_map argument
+    # describes which IDs are found on which bus.
+    transport = moteus_pi3hat.Pi3HatRouter(
+        servo_bus_map = {
+            1:[1,2,3,4,5,6,7,8],
+            # 2:[5,6,7,8],
+        },
+    )
 
-    # In case the controller had faulted previously, at the start of
-    # this script we send the stop command in order to clear it.
-    await c.set_stop()
+    # We create one 'moteus.Controller' instance for each servo.  It
+    # is not strictly required to pass a 'transport' since we do not
+    # intend to use any 'set_*' methods, but it doesn't hurt.
+    #
+    # This syntax is a python "dictionary comprehension":
+    # https://docs.python.org/3/tutorial/datastructures.html#dictionaries
+    servos = {
+        servo_id : moteus.Controller(id=servo_id, transport=transport)
+        for servo_id in [1, 2, 3, 4, 5, 6, 7, 8]
+    }
+
+    # We will start by sending a 'stop' to all servos, in the event
+    # that any had a fault.
+    await transport.cycle([x.make_stop() for x in servos.values()])
 
     while True:
-        # `set_position` accepts an optional keyword argument for each
-        # possible position mode register as described in the moteus
-        # reference manual.  If a given register is omitted, then that
-        # register is omitted from the command itself, with semantics
-        # as described in the reference manual.
+        # The 'cycle' method accepts a list of commands, each of which
+        # is created by calling one of the `make_foo` methods on
+        # Controller.  The most common thing will be the
+        # `make_position` method.
+
+        now = time.time()
+
+        # For now, we will just construct a position command for each
+        # of the 4 servos, each of which consists of a sinusoidal
+        # velocity command starting from wherever the servo was at to
+        # begin with.
         #
-        # The return type of 'set_position' is a moteus.Result type.
-        # It has a __repr__ method, and has a 'values' field which can
-        # be used to examine individual result registers.
-        state = await c.set_position(position=math.nan, query=True)
+        # 'make_position' accepts optional keyword arguments that
+        # correspond to each of the available position mode registers
+        # in the moteus reference manual.
+        commands = [
+            servos[1].make_position(
+                position=math.nan,
+                velocity=0.1*math.sin(now),
+                query=True),
+            servos[2].make_position(
+                position=math.nan,
+                velocity=0.1*math.sin(now + 1),
+                query=True),
+            servos[3].make_position(
+                position=math.nan,
+                velocity=0.1*math.sin(now + 2),
+                query=True),
+            servos[4].make_position(
+                position=math.nan,
+                velocity=0.1*math.sin(now + 3),
+                query=True),
+            servos[5].make_position(
+                position=math.nan,
+                velocity=0.1*math.sin(now + 4),
+                query=True),
+            servos[6].make_position(
+                position=math.nan,
+                velocity=0.1*math.sin(now + 5),
+                query=True),
+            servos[7].make_position(
+                position=math.nan,
+                velocity=0.1*math.sin(now + 6),
+                query=True),
+            servos[8].make_position(
+                position=math.nan,
+                velocity=0.1*math.sin(now + 7),
+                query=True),
+        ]
 
-        # Print out everything.
-        print(state)
+        # By sending all commands to the transport in one go, the
+        # pi3hat can send out commands and retrieve responses
+        # simultaneously from all ports.  It can also pipeline
+        # commands and responses for multiple servos on the same bus.
+        results = await transport.cycle(commands)
 
-        # Print out just the position register.
-        print("Position:", state.values[moteus.Register.POSITION])
+        # The result is a list of 'moteus.Result' types, each of which
+        # identifies the servo it came from, and has a 'values' field
+        # that allows access to individual register results.
+        #
+        # Note: It is possible to not receive responses from all
+        # servos for which a query was requested.
+        #
+        # Here, we'll just print the ID, position, and velocity of
+        # each servo for which a reply was returned.
+        print(", ".join(
+            f"({result.arbitration_id} " +
+            f"{result.values[moteus.Register.POSITION]} " +
+            f"{result.values[moteus.Register.VELOCITY]})"
+            for result in results))
 
-        # And a blank line so we can separate one iteration from the
-        # next.
-        print()
-
-        # Wait 20ms between iterations.  By default, when commanded
-        # over CAN, there is a watchdog which requires commands to be
-        # sent at least every 100ms or the controller will enter a
-        # latched fault state.
+        # We will wait 20ms between cycles.  By default, each servo
+        # has a watchdog timeout, where if no CAN command is received
+        # for 100ms the controller will enter a latched fault state.
         await asyncio.sleep(0.02)
+
 
 if __name__ == '__main__':
     asyncio.run(main())

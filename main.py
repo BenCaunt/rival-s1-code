d4750fcb6218f82cdb287b747d027cc60c429c92
Ben Caunt <bdcaunt@gmail.com>
2024-08-28 23:06:59 -0400

    swerve module calculations

--- main.py
+++ main.py
@@ -22,9 +22,10 @@ bandwidth.
 
 import asyncio
 import math
-import moteus # type: ignore
-import moteus_pi3hat # type: ignore
+import moteus
+import moteus_pi3hat
 import time
+from tqdm import tqdm
 
 
 def angle_wrap(angle):
@@ -36,8 +37,35 @@ def angle_wrap(angle):
 
 AZIMUTH_RATIO = 12.0 / 83.0
 
-def azimuth_angle_to_rotation(azimuth_angle: float) -> float:
-    return azimuth_angle * AZIMUTH_RATIO
+def calculate_swerve_angle(position: float) -> float:
+    """
+    Given the module position in rotations, calculate the angle of the swerve module.
+
+    The swerve module has a 12:83 gear ratio, so the module position is 12/83 of the azimuth
+    position.
+
+    Args:
+        position: The module position in rotations.
+    """
+
+    return angle_wrap(position * 2 * math.pi * AZIMUTH_RATIO)
+
+def calculate_target_position_delta(reference_azimuth_angle, estimated_angle):
+    """
+    Given the reference azimuth angle and the estimated angle of the swerve module, calculate the
+    target position for the module.
+
+    Args:
+        reference_azimuth_angle: The azimuth angle of the robot.
+        estimated_angle: The estimated angle of the swerve module.
+    """
+
+    # Calculate the angle difference between the reference azimuth angle and the estimated angle.
+    angle_difference = angle_wrap(reference_azimuth_angle - estimated_angle)
+
+    # Calculate the target position for the swerve module.
+    return angle_difference / AZIMUTH_RATIO
+
 
 async def main():
     # We will be assuming a system where there are 4 servos, each
@@ -50,6 +78,11 @@ async def main():
             3:[7,8]
         },
     )
+
+    azimuth_ids = [2,4,6,8]
+    drive_ids = [1,3,5,7]
+
+
 
     # We create one 'moteus.Controller' instance for each servo.  It
     # is not strictly required to pass a 'transport' since we do not
@@ -65,6 +98,18 @@ async def main():
     # We will start by sending a 'stop' to all servos, in the event
     # that any had a fault.
     await transport.cycle([x.make_stop() for x in servos.values()])
+
+    position_offsets = {}
+    for id in tqdm(azimuth_ids):
+        position_offsets[id] = 0.0
+        for _ in range(10):
+            results = await transport.cycle([servos[id].make_position(position=math.nan, velocity=0.0, query=True)])
+            for result in results:
+                if result.id == id:
+                    assert result.values[moteus.Register.POSITION] is float
+                    position_offsets[id] += result.values[moteus.Register.POSITION]
+                    break
+        position_offsets[id] /= 10
 
     while True:
         # The 'cycle' method accepts a list of commands, each of which
@@ -84,40 +129,66 @@ async def main():
         # in the moteus reference manual.
 
         gain = 0.1 
-        commands = [
-            servos[1].make_position(
-                position=math.nan,
-                velocity=gain*math.sin(now),
-                query=True),
-            servos[2].make_position(
-                position=math.nan,
-                velocity=gain*math.sin(now + 1),
-                query=True),
-            servos[3].make_position(
-                position=math.nan,
-                velocity=gain*math.sin(now),
-                query=True),
-            servos[4].make_position(
-                position=math.nan,
-                velocity=gain*math.sin(now),
-                query=True),
-            servos[5].make_position(
-                position=math.nan,
-                velocity=gain*math.sin(now + 1),
-                query=True),
-            servos[6].make_position(
-                position=math.nan,
-                velocity=gain*math.sin(now),
-                query=True),       
-            servos[7].make_position(
-                position=math.nan,
-                velocity=gain*math.sin(now),
-                query=True),
-            servos[8].make_position(
-                position=math.nan,
-                velocity=gain*math.sin(now + 1),
-                query=True),         
-        ]
+        # commands = [
+        #     servos[1].make_position(
+        #         position=math.nan,
+        #         velocity=gain*math.sin(now),
+        #         query=True),
+        #     servos[2].make_position(
+        #         position=math.nan,
+        #         velocity=gain*math.sin(now + 1),
+        #         query=True),
+        #     servos[3].make_position(
+        #         position=math.nan,
+        #         velocity=gain*math.sin(now),
+        #         query=True),
+        #     servos[4].make_position(
+        #         position=math.nan,
+        #         velocity=gain*math.sin(now),
+        #         query=True),
+        #     servos[5].make_position(
+        #         position=math.nan,
+        #         velocity=gain*math.sin(now + 1),
+        #         query=True),
+        #     servos[6].make_position(
+        #         position=math.nan,
+        #         velocity=gain*math.sin(now),
+        #         query=True),       
+        #     servos[7].make_position(
+        #         position=math.nan,
+        #         velocity=gain*math.sin(now),
+        #         query=True),
+        #     servos[8].make_position(
+        #         position=math.nan,
+        #         velocity=gain*math.sin(now + 1),
+        #         query=True),         
+        # ]
+
+        reference_angle = math.pi/2 # 90 degrees
+
+
+
+        measured_module_positions = {}
+        for id in azimuth_ids:
+            for result in results:
+                if result.id == id:
+                    measured_module_positions[id] = result.values[moteus.Register.POSITION] - position_offsets[id]
+                    print(f"Measured position for {id}: {measured_module_positions[id]}, {calculate_swerve_angle(measured_module_positions[id])}")
+                    break
+        assert len(measured_module_positions) == len(azimuth_ids)
+
+    
+        for id in azimuth_ids:
+            pos = measured_module_positions[id]
+
+            commands = [
+                servos[id].make_position(
+                    position=pos + calculate_target_position_delta(reference_angle, calculate_swerve_angle(pos)),
+                    velocity=0.0,
+                    query=True)
+            ]
+
+        assert len(commands) == len(azimuth_ids)
 
         # By sending all commands to the transport in one go, the
         # pi3hat can send out commands and retrieve responses
@@ -142,7 +213,7 @@ async def main():
 
         for i,result in enumerate(results):
             print("______________")
-            print(f"{result}")
+            print(f"{result.id}")
             print("______________")
             
 

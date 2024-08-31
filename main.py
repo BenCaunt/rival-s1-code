395fbfc16891380cff1f23272e8d8146244fcf1b
Ben Caunt <bdcaunt@gmail.com>
2024-08-30 21:37:16 -0400

    to degrees

--- main.py
+++ main.py
@@ -85,7 +85,7 @@ async def main():
 
 
                 if id == 2:
-                    print(f"ID: {id}, Current Angle: {math.degrees(current_angle):.2f}° {target_position_delta}, {calculate_swerve_angle(target_position_delta)}", end = " ")
+                    print(f"ID: {id}, Current Angle: {math.degrees(current_angle):.2f}° rotation delta target: {target_position_delta}, --into deg--> {math.degrees(calculate_swerve_angle(target_position_delta))}°", end = " ")
 
                 
             print("")

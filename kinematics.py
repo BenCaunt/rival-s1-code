7b3281b484822a3d49ed520060d2c02b80d3284f
Ben Caunt <bdcaunt@gmail.com>
2024-09-01 17:07:51 -0400

    rotate in place case

--- kinematics.py
+++ kinematics.py
@@ -61,35 +61,52 @@ class ModuleAngles:
 
 def twist_to_wheel_speeds(twist: Twist2dVelocity, dt: float) -> Tuple[WheelSpeeds, ModuleAngles]:
 
+
+    
+
     transform = Transform2d(twist.vx * dt, twist.vy * dt, twist.w * dt)
     twist = transform.log()
     twist = Twist2dVelocity(twist.dx / dt, twist.dy / dt, twist.dyaw / dt)
 
     twist = np.array([twist.vx, twist.vy, twist.w])
 
-    transition = np.array(
-        [
-            [1, 0, -fl_pos.y],
-            [0, 1, fl_pos.x],
-            [1, 0, -fr_pos.y],
-            [0, 1, fr_pos.x],
-            [1, 0, -bl_pos.y],
-            [0, 1, bl_pos.x],
-            [1, 0, -br_pos.y],
-            [0, 1, br_pos.x]
-        ]
-    )
-    speeds = np.dot(transition, twist.transpose())
+    if epsilon_equals(twist[0],0,0.05) and epsilon_equals(twist[1],0,0.05) and not epsilon_equals(twist[2],0,0.05):
+        theta1 = np.deg2rad(-45)
+        theta2 = np.deg2rad(45)
+        theta3 = np.deg2rad(-45)
+        theta4 = np.deg2rad(45)
 
-    v1 = np.sqrt(speeds[0] ** 2 + speeds[1] ** 2)
-    v2 = np.sqrt(speeds[2] ** 2 + speeds[3] ** 2)
-    v3 = np.sqrt(speeds[4] ** 2 + speeds[5] ** 2)
-    v4 = np.sqrt(speeds[6] ** 2 + speeds[7] ** 2)
-    
-    theta1 = np.arctan2(speeds[1], speeds[0])
-    theta2 = np.arctan2(speeds[3], speeds[2])
-    theta3 = np.arctan2(speeds[5], speeds[4])
-    theta4 = np.arctan2(speeds[7], speeds[6])
+        radius = (wheel_base * wheel_base) / 2 # hypotenuse divided by 2 to get distance from center to wheel
+        # w = v / r
+        # v = w * r
+        v1 = twist[2] * radius
+        v2 = twist[2] * radius
+        v3 = twist[2] * radius
+        v4 = twist[2] * radius
+    else:
+        transition = np.array(
+            [
+                [1, 0, -fl_pos.y],
+                [0, 1, fl_pos.x],
+                [1, 0, -fr_pos.y],
+                [0, 1, fr_pos.x],
+                [1, 0, -bl_pos.y],
+                [0, 1, bl_pos.x],
+                [1, 0, -br_pos.y],
+                [0, 1, br_pos.x]
+            ]
+        )
+        speeds = np.dot(transition, twist.transpose())
+
+        v1 = np.sqrt(speeds[0] ** 2 + speeds[1] ** 2)
+        v2 = np.sqrt(speeds[2] ** 2 + speeds[3] ** 2)
+        v3 = np.sqrt(speeds[4] ** 2 + speeds[5] ** 2)
+        v4 = np.sqrt(speeds[6] ** 2 + speeds[7] ** 2)
+        
+        theta1 = np.arctan2(speeds[1], speeds[0])
+        theta2 = np.arctan2(speeds[3], speeds[2])
+        theta3 = np.arctan2(speeds[5], speeds[4])
+        theta4 = np.arctan2(speeds[7], speeds[6])
 
     return WheelSpeeds(v1, v2, v3, v4), ModuleAngles(theta1, theta2, theta3, theta4)
 
@@ -105,3 +122,7 @@ if __name__ == "__main__":
     print(speeds.back_right)
 
 
+
+
+def epsilon_equals(a: float, b: float, epsilon: float) -> bool:
+    return abs(a - b) < epsilon

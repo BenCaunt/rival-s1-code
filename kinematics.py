b143224d0285508a2cddcb61f8231a19e85a2267
Jacob Van Meter <jacobvm04@gmail.com>
2024-09-01 18:36:02 -0400

    get rid of alternate case

--- kinematics.py
+++ kinematics.py
@@ -70,43 +70,43 @@ def twist_to_wheel_speeds(twist: Twist2dVelocity, dt: float) -> Tuple[WheelSpeed
 
     twist = np.array([twist.vx, twist.vy, twist.w])
 
-    if epsilon_equals(twist[0], 0, 0.05) and epsilon_equals(twist[1], 0, 0.05) and not epsilon_equals(twist[2], 0, 0.05):
-        theta1 = np.deg2rad(-45)
-        theta2 = np.deg2rad(45)
-        theta3 = np.deg2rad(-45)
-        theta4 = np.deg2rad(45)
+    # if epsilon_equals(twist[0], 0, 0.05) and epsilon_equals(twist[1], 0, 0.05) and not epsilon_equals(twist[2], 0, 0.05):
+    #     theta1 = np.deg2rad(-45)
+    #     theta2 = np.deg2rad(45)
+    #     theta3 = np.deg2rad(-45)
+    #     theta4 = np.deg2rad(45)
 
-        radius = np.sqrt(wheel_base * wheel_base)  # hypotenuse divided by 2 to get distance from center to wheel
-        # w = v / r
-        # v = w * r
-        v1 = twist[2] * radius
-        v2 = twist[2] * radius
-        v3 = twist[2] * radius
-        v4 = twist[2] * radius
-    else:
-        transition = np.array(
-            [
-                [1, 0, -fl_pos.y],
-                [0, 1, fl_pos.x],
-                [1, 0, -fr_pos.y],
-                [0, 1, fr_pos.x],
-                [1, 0, -bl_pos.y],
-                [0, 1, bl_pos.x],
-                [1, 0, -br_pos.y],
-                [0, 1, br_pos.x],
-            ]
-        )
-        speeds = np.dot(transition, twist.transpose())
+    #     radius = np.sqrt(wheel_base * wheel_base)  # hypotenuse divided by 2 to get distance from center to wheel
+    #     # w = v / r
+    #     # v = w * r
+    #     v1 = twist[2] * radius
+    #     v2 = twist[2] * radius
+    #     v3 = twist[2] * radius
+    #     v4 = twist[2] * radius
+    # else:
+    transition = np.array(
+        [
+            [1, 0, -fl_pos.y],
+            [0, 1, fl_pos.x],
+            [1, 0, -fr_pos.y],
+            [0, 1, fr_pos.x],
+            [1, 0, -bl_pos.y],
+            [0, 1, bl_pos.x],
+            [1, 0, -br_pos.y],
+            [0, 1, br_pos.x],
+        ]
+    )
+    speeds = np.dot(transition, twist.transpose())
 
-        v1 = np.sqrt(speeds[0] ** 2 + speeds[1] ** 2)
-        v2 = np.sqrt(speeds[2] ** 2 + speeds[3] ** 2)
-        v3 = np.sqrt(speeds[4] ** 2 + speeds[5] ** 2)
-        v4 = np.sqrt(speeds[6] ** 2 + speeds[7] ** 2)
+    v1 = np.sqrt(speeds[0] ** 2 + speeds[1] ** 2)
+    v2 = np.sqrt(speeds[2] ** 2 + speeds[3] ** 2)
+    v3 = np.sqrt(speeds[4] ** 2 + speeds[5] ** 2)
+    v4 = np.sqrt(speeds[6] ** 2 + speeds[7] ** 2)
 
-        theta1 = np.arctan2(speeds[1], speeds[0])
-        theta2 = np.arctan2(speeds[3], speeds[2])
-        theta3 = np.arctan2(speeds[5], speeds[4])
-        theta4 = np.arctan2(speeds[7], speeds[6])
+    theta1 = np.arctan2(speeds[1], speeds[0])
+    theta2 = np.arctan2(speeds[3], speeds[2])
+    theta3 = np.arctan2(speeds[5], speeds[4])
+    theta4 = np.arctan2(speeds[7], speeds[6])
 
     return WheelSpeeds(front_left=v1, front_right=v2, back_left=v3, back_right=v4), ModuleAngles(
         front_left_angle=theta1, front_right_angle=theta2, back_left_angle=theta3, back_right_angle=theta4
